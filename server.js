b4540badf3bdae98645284a1c14dcbc7f2dea5aa
tonyjohnvan <tonyjohnvan@gmail.com>
2016-10-26 19:31:44 -0400

    mongod basic

--- server.js
+++ server.js
@@ -1,23 +1,50 @@
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+// MongoDB
+const MongoClient = require('mongodb').MongoClient
+var db
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', express.static(__dirname + '/public'));
+MongoClient.connect('mongodb://localhost:27017/mongodemo', (err, database) =>{
+
+  if(err) return console.log(err)
+
+  db = database
+
+  // setup
+  app.listen(5008, () => {
+    console.log('listen on 5008')
+  })
+
+  app.post('/quotes', (req, res) => {
+    console.log(req.body)
+
+    database.collection('quotes').insertOne(req.body, (err, result) => {
+      if (err) return console.log(err)
+
+      console.log('saved to database')
+
+      res.sendStatus(204)
+    })
+
+  })
+
+  app.get('/quotes', (req, res) => {
+    var cursor = db.collection('quotes').find({}).toArray((err,result)=>{
+      console.log(result)
+      res.send(result)
+    })
+  })
+
+})
+
+app.get('/', express.static(__dirname + '/public'))
 
 // routes
-app.get('/hello', function (req, res) {
+app.get('/hello', (req, res) => {
   res.send('Hello, world')
-})
-
-app.post('/quotes', function (req, res) {
-  console.log(req.body)
-  res.sendStatus(204)
-})
-
-// setup
-app.listen(5008, function(){
-  console.log('listen on 5008')
 })
