e72c59784005b7c9e858c484b674e6da0e79ddd8
zhaokun.long <zhaokun.long@2dm.com.au>
2019-10-18 10:54:34 +0800

    读取文件 将 文件内容清理然后打入爬虫库

--- utill/readerCsv.js
+++ utill/readerCsv.js
@@ -1,7 +1,8 @@
-const csvFilePath = './utill/002.csv'
+const csvFilePath = './utill/003.csv'
 const csv = require('csvtojson')
-// const loadJsonFile = require('load-json-file');
+const loadJsonFile = require('load-json-file');
 const writeJsonFile = require('write-json-file');
+const API = require('./api.js')
 var translate = require("translate")
 
 
@@ -17,32 +18,72 @@ function getCsv() {
     })
 }
 
-// async function getJSONFile() {
-//     const jsonFile = './utill/zh.js'
-//     const jsonData = await loadJsonFile(jsonFile);
-//     return jsonData
-// }
+async function getJSONFile(jsonFile) {
+    const File = jsonFile || './utill/zh.js'
+    const jsonData = await loadJsonFile(File);
+    return jsonData
+}
 
-async function writeJSONFiel(json){
-    const data =  await writeJsonFile('./utill/en.js', json);
+async function writeJSONFiel(fileurl, json) {
+    const file = fileurl || './utill/en.js'
+    const data = await writeJsonFile(file, json);
     console.log('down')
 }
 
+function regxForUrl(url) {
+    const regex = new RegExp(/http(s)?:\/\/(www|m).youtube.com\/((channel|c|user)\/)+([a-zA-Z0-9-_.]+)\/?(.*)/, 'g');
+    return regex.exec(url);
+}
+
+const sendArry = []
+const keywords = []
+function manyInster() {
+    getCsv().then((res) => {
+        res.forEach(element => {
+            const temp = {
+                channelUrl: element['channel'],
+                tags: `StreetAddress,${element['Street Address']},PhoneNO.,${element['Phone NO.']},Country,${element['Country']} city,${element['City']}`,
+                level: 2,
+                platform: 'youtube',
+                email: element['Customer Email'],
+                fansSum: element['SKU(标明数量)']
+            }
+            let channelUrl = regxForUrl(temp.channelUrl)
+            if (channelUrl) {
+                sendArry.push(temp)
+            } else {
+                keywords.push(temp)
+            }
+        });
+        console.log(sendArry)
+        console.log('===================================================')
+        console.log(keywords.length)
+        // requestAddDict(sendArry)
+        
+    })
+}
+
+async function requestAddDict(json) {
+    console.log('===================================================')
+    json.forEach( (e)=>{
+        API.addDicts(e).then((res)=>{
+        }).catch(()=>{
+            
+        })
+    })
+}
+
+// getJSONFile('./utill/keywords.js').then(res =>{
+//     requestAddDict(res)
+
+// })
+
+async function saveKeyword(json) {
+    await writeJSONFiel('./utill/keywords.js', json)
+}
 
 
-getCsv().then((res)=>{
-    const sendArry = []
-    res.forEach(element => {
-        const temp = {
-            channelUrl : element['链接'],
-            tags: `${element['视频类别'].split(/\s|,|、/).toString()},负责的产品,${element['负责产品'].split(/\s|,|、/).toString()}`
-        }
-        if(temp.channelUrl){
-            sendArry.push(temp)
-        }
-    });
-    console.log(sendArry)
-})
+manyInster()
 
 module.exports = {
     getCsv,
